4579fd22114ca564d3b3eb1d1523bdf5c3e5ef4a
Warren Hamdi <warren.hamdi@gmail.com>
2025-11-18 14:19:02 +0100

    [Frontend] -> created waiting list page

--- frontend/vite-achyl/src/App.tsx
+++ frontend/vite-achyl/src/App.tsx
@@ -1,17 +1,88 @@
 import './App.css'
+import './index.css'
 
+const CallToAction = () => {
+  return (
+    <div className="max-w-7xl p-6 lg:px-8">
+      <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 shadow-2xl rounded-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+        <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          Join the Waitlist !
+        </h2>
 
+        <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
+          Archyl is managing your flow of informations and keeping tracks on your memo, links and more ..
+        </p>
+        <form className="mx-auto mt-10 flex max-w-md gap-x-4">
+          <label htmlFor="email-address" className="sr-only">
+            Email address
+          </label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+            className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+            placeholder="Enter your email"
+          />
+
+          <button
+            type="submit"
+            className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+          >
+            Notify me
+          </button>
+        </form>
+
+        {/* If the SVG is decorative, ensure it's accessible. */}
+        <svg
+          viewBox="0 0 1024 1024"
+          className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
+          aria-hidden="true"
+        >
+          <circle
+            cx="512"
+            cy="512"
+            r="512"
+            fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+            fillOpacity="0.7"
+          />
+          <defs>
+            <radialGradient
+              id="759c1415-0410-454c-8f7c-9a820de03641"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(512 512) rotate(90) scale(512)"
+            >
+              <stop stopColor="#7775D6" />
+              <stop offset="1" stopColor="#7ED321" stopOpacity="0" />
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 p-6">
+      <h1 className="font-display text-blue-950 text-2xl font-bold">ARCHYL</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-[#F0EAD6] flex items-center justify-center">
-      <div className="bg-[#8496CF]  w-[170vmin] h-[90vmin] flex items-center justify-center rounded-lg">
-        <p className="">
-          Hello World
-        </p>
+    <div className="relative h-screen w-screen">
+      <Header />
+
+      <div className="flex h-full w-full items-center justify-center">
+        <CallToAction />
       </div>
     </div>
   );
 }
-
 export default App;
